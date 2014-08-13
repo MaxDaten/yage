7c98611b6876eee6c7e5a1b4fe2fc264d547851c
Jan-Philip Loos <maxdaten@gmail.com>
2014-08-14 01:59:28 +0200

    fixing radius for sdf

--- src/Yage/Image/SDF.hs
+++ src/Yage/Image/SDF.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Yage.Image.SDF
     ( signedNearDistance
     , signedDistanceFieldProgressed
@@ -11,8 +12,7 @@ import Control.Monad.ST
 import Codec.Picture
 import Codec.Picture.Types
 
-data InOut = Inside | Outside
-    deriving (Show, Eq)
+
 
 
 signedDistanceFieldProgressed :: Image Pixel8 -> Int -> (Int -> IO ()) -> IO (Image Pixel8)
@@ -23,7 +23,7 @@ signedDistanceFieldProgressed bitmap spread incrCall = do
     img <- createMutableImage w h (minBound :: Pixel8)
     forM_ [0..h-1] $ \y -> do
         forM_ [0..w-1] $ \x ->
-            writePixel img x y (signedNearDistance bitmap spread x y)
+            writePixel img x y $ signedNearDistance bitmap spread x y
         incrCall w
 
     unsafeFreezeImage img
@@ -42,35 +42,49 @@ signedDistanceField bitmap spread = runST $ do
     unsafeFreezeImage img
 
 
+data InOut = Inside | Outside
+    deriving (Show, Eq)
+
+
 signedNearDistance :: Image Pixel8 -> Int -> Int -> Int -> Pixel8
 signedNearDistance bitmap spread x y =
-    let s, upperPx, maxDist :: Double
+    let s, upperPx, maxDist2 :: Double
         s         = fromIntegral spread
-        maxDist   = sqrt $ 2 * s * s
+        maxDist2  = s * s
         upperPx   = fromIntegral (maxBound :: Pixel8)
+        halfPx    = upperPx / 2.0
 
         msqr      = headMay $ sort distSqr's
         signing   = if activeSide == Inside then id else negate
-        dist      = maybe (signing maxDist) (signing . sqrt . fromIntegral) msqr
-        val       = round $ (dist + maxDist) / (2.0 * maxDist) * upperPx
+        dist      = signing $ sqrt $ maybe maxDist2 fromIntegral msqr
+        --val       = round $ dist * ( half / s ) + half
+        --val       = round $ (dist + s) / ( 2.0 * s ) * upperPx
+        val       = round $ dist / s * halfPx + halfPx
     in clamp val minBound maxBound
 
     where
 
     activeSide      = toInside $ pixelAt bitmap x y
+    {-# INLINE activeSide #-}
+
     toInside px     = if px == maxBound then Inside else Outside
-    -- isInside x y    = toInside (pixelAt bitmap x y) == Inside
+    {-# INLINE toInside #-}
 
-    distSqr's = [ dx*dx + dy*dy | (dx,dy) <- others ]
-
-    others = do
+    distSqr's       = do
         offx <- [ -spread .. spread ]
         offy <- [ -spread .. spread ]
+        guard $ (offx, offy) /= (0,0)
+
+        -- out of radius
+        let distSqr = offx * offx + offy * offy
+        guard $ distSqr <= spread * spread
+
         let xi = x + offx
             yi = y + offy
-        guard $ (offx, offy) /= (0,0)
         guard $ xi >= 0 && xi < imageWidth bitmap
         guard $ yi >= 0 && yi < imageHeight bitmap
-        -- is on the other side?
+
+        -- pixel is on the other side?
         guard $ toInside (pixelAt bitmap xi yi) /= activeSide
-        return (offx, offy)
+        return distSqr
+{-# INLINE signedNearDistance #-}
