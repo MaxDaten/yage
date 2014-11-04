0c6f8804be921d55973a3cb079ad1a2dc0254638
Jan-Philip Loos <maxdaten@gmail.com>
2014-11-04 23:18:20 +0100

    doc

--- src/Yage/Wire/Utils.hs
+++ src/Yage/Wire/Utils.hs
@@ -4,12 +4,16 @@
 module Yage.Wire.Utils where
 
 import Yage.Prelude
+import Yage.Math
 import Control.Wire
 
-
+-- | a wire to adjust values between a range based on a up and
+-- down 'Event'. The 'Event' carries the signed increment/decrement value.
+-- 'x': the starting value
+-- '(minV,maxV)': the allowed range for the resulting value
 spin :: (Num v, Ord v, Monad m, Monoid e) => (v,v) -> v -> Wire s e m (Event v, Event v) v
 spin (minV, maxV) x = proc (up, down) ->
-    accumHold x -< (\f -> max minV . min maxV . f) <$> mergeL ((+) <$> up) (flip (-) <$> down)
+    accumHold x -< (\a b -> clamp (a + b) minV maxV) <$> mergeL up down
 
 
 accumHold :: (Monad m, Monoid e) => a -> Wire s e m (Event (a -> a)) a
