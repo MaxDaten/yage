5c748442d7e155b8718d1bee4b7002895d1a7db6
Jan-Philip Loos <maxdaten@gmail.com>
2014-12-10 11:50:53 +0100

    indent

--- src/Yage/Image.hs
+++ src/Yage/Image.hs
@@ -5,16 +5,14 @@ module Yage.Image
   , module Img
   ) where
 
-import           Yage.Lens
-import           Yage.Math
 import           Yage.Prelude
 
+import           Yage.Lens
+import           Yage.Math
 import           Yage.Color
-
 import           Codec.Picture              as Img
 import           JuicySRGB                  as Color
 import           Quine.Image                as Img
-
 import           Yage.Geometry.D2.Rectangle
 
 type ImageDimension = V2 Int
@@ -26,47 +24,38 @@ instance GetRectangle (Image p) Int where
 constImage :: Pixel a => ImageDimension -> a -> Image a
 constImage (V2 w h) value = generateImage (const . const $ value) w h
 
-
 constColorImage :: ( RealFrac a, Floating a, ColourPixel a p ) => ImageDimension -> Colour a -> Image p
 constColorImage dim = constImage dim . colourToPixel
-
 
 constPx :: Pixel a => a -> Image a
 constPx = constImage 1
 
-
 constColorPx :: ( RealFrac a, Floating a, ColourPixel a p ) => Colour a -> Image p
 constColorPx = constColorImage 1
 
-
-
 subImage :: (Pixel a) => Image a -> ImagePosition -> Image a -> Image a
 subImage sub atPx@(V2 atX atY) target
-    | not subImageFit = error $ unpack $ format "sub image does not fit at \"{}\" in target image" (Only $ Shown atPx)
-    | otherwise   = generateImage includeRegionImg (imageWidth target) (imageHeight target)
-    where
-        includeRegionImg px py =
-          if subRegion `containsPoint` (fromIntegral <$> V2 px py)
-            -- pixel is sourced from sub image
-            then pixelAt sub (px - subRegion^.width) (py - subRegion^.height)
-            -- pixel is sourced from target image
-            else pixelAt target px py
+  | not subImageFit = error $ unpack $ format "sub image does not fit at \"{}\" in target image" (Only $ Shown atPx)
+  | otherwise   = generateImage includeRegionImg (imageWidth target) (imageHeight target)
+  where
+  includeRegionImg px py =
+    if subRegion `containsPoint` (fromIntegral <$> V2 px py)
+    -- pixel is sourced from sub image
+    then pixelAt sub (px - subRegion^.width) (py - subRegion^.height)
+    -- pixel is sourced from target image
+    else pixelAt target px py
 
-        subRegion :: Rectangle Int
-        subRegion = (sub^.asRectangle) `translate` atPx
-        subImageFit
-          = imageWidth target  >= imageWidth sub + atX  &&
-            imageHeight target >= imageHeight sub + atY
-
+  subRegion :: Rectangle Int
+  subRegion = (sub^.asRectangle) `translate` atPx
+  subImageFit
+    = imageWidth target  >= imageWidth sub + atX  &&
+      imageHeight target >= imageHeight sub + atY
 
 imageDimension :: Image a -> ImageDimension
 imageDimension img = V2 (imageWidth img - 1) (imageHeight img - 1)
 
-
-
 imageRectangle :: Image a -> Rectangle Int
 imageRectangle img = Rectangle 0 $ imageDimension img
-
 
 imageByAreaCompare :: Image a -> Image a -> Ordering
 imageByAreaCompare = compare `on` (^.to imageRectangle.area)
