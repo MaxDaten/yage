41561d7c6d988d88687b56cf08dde932c700973e
Jan-Philip Loos <maxdaten@gmail.com>
2014-12-10 08:05:13 +0100

    added Classy and Fields for transformation

--- src/Yage/Transformation.hs
+++ src/Yage/Transformation.hs
@@ -1,90 +1,79 @@
 {-# LANGUAGE MultiWayIf      #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Yage.Transformation where
 
-import           Control.Applicative
-import qualified Linear              (lerp, slerp)
+import           Yage.Prelude
 import           Yage.Lens
 import           Yage.Math           hiding (lerp)
-import           Yage.Prelude
+
+import           Control.Applicative
+import           Data.Data
+import qualified Linear              (lerp, slerp)
 
 data Transformation a = Transformation
-    { _transPosition    :: !(V3 a)
-    , _transOrientation :: !(Quaternion a)
-    , _transScale       :: !(V3 a)
-    } deriving ( Show, Eq, Ord, Typeable, Functor )
+  { _transformationPosition    :: !(V3 a)
+  , _transformationOrientation :: !(Quaternion a)
+  , _transformationScale       :: !(V3 a)
+  } deriving (Show,Eq,Ord,Functor,Traversable,Foldable,Data,Typeable,Generic)
 
 
-makeLenses ''Transformation
+-- makeLenses ''Transformation
+makeFields ''Transformation
+makeClassy ''Transformation
 
 idTransformation :: RealFloat a => Transformation a
 idTransformation = Transformation 0 1 1
 
 
-transformationMatrix :: Num a => Getter (Transformation a) (M44 a)
+transformationMatrix :: (Num a, HasPosition t (V3 a), HasScale t (V3 a), HasOrientation t (Quaternion a)) => Getter t (M44 a)
 transformationMatrix = to get where
-    get trans =
-        let scaleM       = kronecker . point $ trans^.transScale
-            transM       = mkTransformation (trans^.transOrientation) (trans^.transPosition)
-        in transM !*! scaleM
+  get trans =
+    let scaleM       = kronecker . point $ trans^.scale
+        transM       = mkTransformation (trans^.orientation) (trans^.position)
+    in transM !*! scaleM
 
 
-inverseTransformation :: (Conjugate a, RealFloat a) => Transformation a -> Transformation a
+inverseTransformation :: (Conjugate a, RealFloat a, HasTransformation t a) => t -> t
 inverseTransformation t =
-    t   & transScale            %~ recip
-        & transPosition         %~ negate
-        & transOrientation._ijk %~ negate
-
-
--- | Creates a Quaternion from a orthonormalized vector space
---   and a normalized look-at direction vector
-lookAt :: (Epsilon a, RealFloat a, Show a) => M33 a -> V3 a -> Quaternion a
-lookAt (V3 _wx wy wz) directionVector =
-    let axis   = normalize $ wy `cross` directionVector
-        theta  = wy `dot` directionVector
-    in if | nearZero (theta - 1) -> 1
-          | nearZero (theta + 1) -> axisAngle wz pi
-          | otherwise            -> axisAngle axis (acos theta)
-{-# SPECIALISE INLINE lookAt :: M33 Float -> V3 Float -> Quaternion Float #-}
-{-# SPECIALISE INLINE lookAt :: M33 Double -> V3 Double -> Quaternion Double #-}
+  t & transformation.scale            %~ recip
+    & transformation.position         %~ negate
+    & transformation.orientation._ijk %~ negate
 
 
 instance Applicative Transformation where
-    pure a = Transformation (pure a) (pure a) (pure a)
-    (Transformation fp fo fs) <*> (Transformation p o s) = Transformation (fp <*> p) (fo <*> o) (fs <*> s)
+  pure a = Transformation (pure a) (pure a) (pure a)
+  (Transformation fp fo fs) <*> (Transformation p o s) = Transformation (fp <*> p) (fo <*> o) (fs <*> s)
 
 instance RealFloat a => Num (Transformation a) where
-    (+) = liftA2 (+)
-    (-) = liftA2 (-)
-    (*) = liftA2 (*)
-    negate = fmap negate
-    abs = fmap abs
-    signum = fmap signum
-    fromInteger = pure . fromInteger
+  (+) = liftA2 (+)
+  (-) = liftA2 (-)
+  (*) = liftA2 (*)
+  negate = fmap negate
+  abs = fmap abs
+  signum = fmap signum
+  fromInteger = pure . fromInteger
 
 
 instance (RealFloat a, Epsilon a) => Epsilon (Transformation a) where
-    nearZero (Transformation p o s) = nearZero p && nearZero o && nearZero s
+  nearZero (Transformation p o s) = nearZero p && nearZero o && nearZero s
 
 instance RealFloat a => Default (Transformation a) where
-    def = idTransformation
-
+  def = idTransformation
 
 class LinearInterpolatable a where
-    lerp :: Double -> a -> a -> a
-
+  lerp :: Double -> a -> a -> a
 
 instance RealFloat a => LinearInterpolatable (Transformation a) where
-    lerp alpha u v = u & transPosition    .~ Linear.lerp (realToFrac alpha) (u^.transPosition) (v^.transPosition)
-                       & transScale       .~ Linear.lerp (realToFrac alpha) (u^.transScale) (v^.transScale)
-                       & transOrientation .~ Linear.slerp (u^.transOrientation) (v^.transOrientation) (realToFrac alpha)
-    {-# INLINE lerp #-}
-
+  lerp alpha u v = u & position     .~ Linear.lerp (realToFrac alpha) (u^.position) (v^.position)
+                     & scale        .~ Linear.lerp (realToFrac alpha) (u^.scale) (v^.scale)
+                     & orientation  .~ Linear.slerp (u^.orientation) (v^.orientation) (realToFrac alpha)
+  {-# INLINE lerp #-}
 
 instance LinearInterpolatable Float where
-    lerp alpha u v = (Linear.lerp (realToFrac alpha) (V1 u) (V1 v))^._x
-    {-# INLINE lerp #-}
+  lerp alpha u v = (Linear.lerp (realToFrac alpha) (V1 u) (V1 v))^._x
+  {-# INLINE lerp #-}
 
 instance LinearInterpolatable Double where
-    lerp alpha u v = (Linear.lerp (realToFrac alpha) (V1 u) (V1 v))^._x
-    {-# INLINE lerp #-}
+  lerp alpha u v = (Linear.lerp (realToFrac alpha) (V1 u) (V1 v))^._x
+  {-# INLINE lerp #-}
