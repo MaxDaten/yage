d97d1aeae6092e3ba95bfd3fc3359f37d2580522
Jan-Philip Loos <maxdaten@gmail.com>
2014-12-17 16:08:37 +0100

    added bindTexture

--- src/Yage/Rendering/Resources/GL/Texture.hs
+++ src/Yage/Rendering/Resources/GL/Texture.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts    #-}
 module Yage.Rendering.Resources.GL.Texture (
     module Img
   , Texture(..)
   , createTexture2D
   , createTexture2DImage
   , resizeTexture2D
+  , bindTexture
   ) where
 
 import           Yage.Prelude
@@ -38,10 +40,12 @@ createTexture2DImage :: Image2D i => GL.TextureTarget -> i -> Acquire (Texture a
 createTexture2DImage target img = do
   tex <- glResource
   GL.boundTexture target GL_TEXTURE_BINDING_2D $= tex
+  GL.texParameteri GL_TEXTURE_2D GL_TEXTURE_BASE_LEVEL $= 0
+  GL.texParameteri GL_TEXTURE_2D GL_TEXTURE_MAX_LEVEL $= 0
   upload img target 0
   return $ Texture target Texture2D tex
 
--- | Creates an uninitialized 'Texture' with 'ImageFormat' derived from a proxy
+-- | Creates an uninitialized 'Texture' with 'ImageFormat' derived from the result type
 createTexture2D :: ImageFormat px => GL.TextureTarget -> Int -> Int -> Acquire (Texture px)
 createTexture2D target width height = do
   tex <- glResource
@@ -53,6 +57,15 @@ resizeTexture2D tex@(Texture target _ obj) width height = do
   GL.boundTexture target GL_TEXTURE_BINDING_2D $= obj
   glTexImage2D target 0 (internalFormat tex) (fromIntegral width) (fromIntegral height) 0 (pixelFormat tex) (pixelType tex) nullPtr
   return tex
+
+
+bindTexture :: HasGetter (g Int32) Int32 => GL.TextureTarget -> g Int32 -> SettableStateVar (Maybe (Texture a))
+bindTexture target st = SettableStateVar s where
+  s mtex = do
+    unit <- get st
+    GL.activeTexture $= fromIntegral unit
+    GL.boundTexture target 0 $= maybe def (\(Texture _ _ obj) -> obj) mtex
+
 
 instance FramebufferAttachment (Texture a) where
   attach target slot (Texture _ _ obj) = attach target slot obj
