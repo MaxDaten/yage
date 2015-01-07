a2bf3f40dfb0c1852276f74b0434705f2a27a4fa
Jan-Philip Loos <maxdaten@gmail.com>
2015-01-07 17:12:09 +0100

    some 'excuse' for breaking lens laws
    will be fixed (i promise!)

--- src/Yage/Vertex.hs
+++ src/Yage/Vertex.hs
@@ -13,6 +13,7 @@ module Yage.Vertex
   ) where
 
 import Yage.Lens
+-- import Quine.GL.Attribute (Layout)
 
 -- | 'HasLayout' is just a type level proxy for vertex data objects.
 -- It allows the annotation of a OpenGL compatible 'Layout' to the
@@ -33,3 +34,17 @@ makeFields ''Position
 makeFields ''Texture
 makeFields ''Normal
 makeFields ''Tangent
+
+-- | current implementation example
+-- FIXME: major problem here is, it violates the first lens law (you get what you set). you cant set currently any layout, this could be missleading
+-- subject for a rework
+{--
+instance HasPosition YGMVertexLayout Layout where
+  position = lens (const $ Layout 3 GL_FLOAT False (sizeOf (undefined::YGMVertex)) (nullPtr)) (const)
+instance HasTexture (HasLayout YGMVertex) Layout where
+  texture = lens (const $ Layout 2 GL_FLOAT False (sizeOf (undefined::YGMVertex)) (nullPtr `plusPtr` (sizeOf (undefined::Vec3)))) (const)
+instance HasTangentX (HasLayout YGMVertex) Layout where
+  tangentX = lens (const $ Layout 3 GL_FLOAT False (sizeOf (undefined::YGMVertex)) (nullPtr `plusPtr` (sizeOf (undefined::Vec3) + sizeOf (undefined::Vec2)))) (const)
+instance HasTangentZ (HasLayout YGMVertex) Layout where
+  tangentZ = lens (const $ Layout 4 GL_FLOAT False (sizeOf (undefined::YGMVertex)) (nullPtr `plusPtr` (sizeOf (undefined::Vec3) + sizeOf (undefined::Vec2) + sizeOf (undefined::Vec3)))) (const)
+--}
