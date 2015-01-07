15ff32cca5ceb9e73cb10d6be55ba2fae9ad2a13
Jan-Philip Loos <maxdaten@gmail.com>
2015-01-07 05:08:15 +0100

    convinience resources

--- src/Yage/Texture.hs
+++ src/Yage/Texture.hs
@@ -2,6 +2,7 @@
 module Yage.Texture
   ( module Yage.Image
   , materialRes
+  , texture2DRes
   ) where
 
 import Yage.Prelude
@@ -12,7 +13,12 @@ import Yage.Material
 import Yage.Resources
 import Yage.Rendering.Resources.GL.Texture
 import Yage.Rendering.GL
+import Yage.Geometry.D2.Rectangle
 
-materialRes :: Image2D (Image px) => Material col (Image px) -> YageResource (Material col (Texture px))
+
+materialRes :: (GetRectangle i Int, Image2D i) => Material col i -> YageResource (Material col (Texture px))
 materialRes imgMat = (set materialTexture) <$> (createTexture2DImage GL_TEXTURE_2D (imgMat^.materialTexture)) <*> pure imgMat
+
+texture2DRes :: (GetRectangle i Int, Image2D i) => i -> YageResource (Texture px)
+texture2DRes = createTexture2DImage GL_TEXTURE_2D
 
