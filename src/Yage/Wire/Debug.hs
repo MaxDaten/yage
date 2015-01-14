2cc733cad8f07237e052f514a67dc64666d1f48d
Jan-Philip Loos <maxdaten@gmail.com>
2015-01-14 20:44:41 +0100

    indention

--- src/Yage/Wire/Debug.hs
+++ src/Yage/Wire/Debug.hs
@@ -8,7 +8,7 @@ import Control.Wire
 -- | traces the input signal with the (local) time stamp
 traceW :: (HasTime t s, Show a, Show t, Real t, MonadIO m) => Wire s e m a a
 traceW = proc x -> do
-    t <- time -< ()
-    printW -< (t, x)
-    where
-    printW = mkGen_ $ \(t,x) -> printTF "{}:{}\n" (fixed 4 t, Shown x) >> return (Right x)
+  t <- time -< ()
+  printW -< (t, x)
+  where
+  printW = mkGen_ $ \(t,x) -> printTF "{}:{}\n" (fixed 4 t, Shown x) >> return (Right x)
