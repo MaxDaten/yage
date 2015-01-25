cc337ba601c36dee5759b9a9028fcde83e3f7fa7
Jan-Philip Loos <maxdaten@gmail.com>
2015-01-25 01:48:23 +0100

    adopt new RenderSystem for Bloom

--- src/Yage/Rendering/Pipeline/Deferred/Bloom.hs
+++ src/Yage/Rendering/Pipeline/Deferred/Bloom.hs
@@ -1,47 +1,45 @@
 {-# LANGUAGE TupleSections          #-}
+{-# LANGUAGE Arrows                 #-}
 module Yage.Rendering.Pipeline.Deferred.Bloom
   ( addBloom
   ) where
 
 
 import           Yage.Prelude hiding ((</>), foldM, cons, (++))
-import           Yage.Lens hiding (cons)
-import           Yage.Vertex hiding (Texture)
-import           Yage.Formats.Ygm
+import           Yage.Lens
 
-
-import           Yage.HDR
-import           Yage.Rendering.GL
 import           Yage.Rendering.RenderSystem                     as RenderSystem
 import           Yage.Rendering.Resources.GL
 import           Yage.Scene
-import           Yage.Viewport
-import           Yage.Material
 
-import           Yage.Rendering.Pipeline.Deferred.Common         as Pass
 import           Yage.Rendering.Pipeline.Deferred.Downsampling   as Pass
 import           Yage.Rendering.Pipeline.Deferred.GaussianBlur   as Pass
 import           Yage.Rendering.Pipeline.Deferred.LuminanceFilter   as Pass
 
-import           System.FilePath ((</>))
-import           Control.Monad (foldM)
-import           Data.List ((++))
 import           Data.Maybe (fromJust)
-import           Quine.GL.Types
 
 
-addBloom :: ImageFormat px => Int -> YageResource (RenderSystem (Float,Texture px) (Texture px))
+-- redundancy Yage.Rendering.Pipeline.Deferred.GaussianBlur.blurPass will be fixed with 'YageResource' factored out
+addBloom :: (ImageFormat px, MonadResource m) => Int -> YageResource (Pass m (Float,Texture px) (Texture px))
 addBloom numSamples = do
-  sceneHalf  <- lmap (2,) <$> downsampler
+  sceneHalf         <- lmap (2,) <$> downsampler
   halfSamplers      <- replicateM numSamples $ lmap (2,) <$> downsampler
   gaussianSamplers  <- replicateM (numSamples + 1) $ gaussianSampler
   filterLuma <- luminanceFilter
-  return $ do
-    (thrshold, inTex) <- ask
-    filtered <- filterLuma . fmap (thrshold,) sceneHalf . pure inTex
-    downsampledTextures <- reverse <$> foldM processDownsample [(2::Int,filtered)] halfSamplers
-    fromJust <$> foldM (\a (gaussian,(_,d)) -> Just <$> gaussian . pure (d,a)) Nothing (zip gaussianSamplers downsampledTextures)
+  return $ proc (thrshold, inTex) -> do
+    half     <- sceneHalf -< inTex
+    filtered <- filterLuma -< (thrshold,half)
+    downsampledTextures <- processDownsamples halfSamplers -< [(2::Int,filtered)]
+    processGauss gaussianSamplers -< (map snd downsampledTextures, Nothing)
  where
-  processDownsample txs dsampler =
-    let (lastfactor, base) = unsafeLast txs
-    in fmap ((++) txs . singleton . (2*lastfactor,)) dsampler . pure base
+  processGauss :: Monad m => [Pass m (Texture px, Maybe (Texture px)) (Texture px)] -> Pass m ([Texture px], Maybe (Texture px)) (Texture px)
+  processGauss [] = rmap (fromJust.snd) id
+  processGauss (s:ss) = proc ((t:texs), madd) -> do
+    out <- s -< (t, madd)
+    processGauss ss -< (texs, Just out)
+
+  processDownsamples :: Monad m => [Pass m (Texture px) (Texture px)] -> Pass m [(Int,Texture px)] [(Int,Texture px)]
+  processDownsamples [] = id
+  processDownsamples (s:ss) = proc (t:texs) -> do
+    tex <- s -< snd t
+    processDownsamples ss -< (2 * fst t,tex):t:texs
