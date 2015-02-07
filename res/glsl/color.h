b80743b5be8a5c8c2740479d4c031741627dc921
Jan-Philip Loos <maxdaten@gmail.com>
2015-02-07 20:25:01 +0100

    added color correction from SRGB to Linear

--- res/glsl/color.h
+++ res/glsl/color.h
@@ -9,6 +9,20 @@ float Luminance(vec3 color)
   // return dot( color, vec3( 0.3, 0.59, 0.11 ) );
 }
 
+// [http://chilliant.blogspot.de/2012/08/srgb-approximations-for-hlsl.html]
+vec3 sRRGBToLinear(vec3 sRGB)
+{
+  return sRGB * (sRGB * (sRGB * 0.305306011 + 0.682171111) + 0.012522878);
+}
+
+vec4 sRRGBToLinear(vec4 sRGBA)
+{
+  vec4 rgba;
+  rgba.rgb = sRRGBToLinear(sRGBA.rgb);
+  rgba.a   = sRGBA.a;
+  return rgba;
+}
+
 vec4 gamma(vec3 x, float y)
 {
     return vec4(pow(x.r, y), pow(x.g, y), pow(x.b, y), 1.0);
