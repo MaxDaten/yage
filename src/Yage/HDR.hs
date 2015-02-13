64d589d1de722e4046d502687b3fb69934364ad3
Jan-Philip Loos <maxdaten@gmail.com>
2015-02-13 04:55:56 +0100

    add HDRSensor

--- src/Yage/HDR.hs
+++ src/Yage/HDR.hs
@@ -13,64 +13,82 @@ import Data.Data
 
 
 data HDRCamera = HDRCamera
-    { _hdrCameraHandle  :: Camera
-    , _hdrExposure      :: Double
-    , _hdrExposureBias  :: Double
-    , _hdrWhitePoint    :: Double
-    , _hdrBloomSettings :: HDRBloomSettings
-    } deriving (Show,Eq,Ord,Data,Typeable,Generic)
+  { _hdrCameraHandle  :: Camera
+  , _hdrCameraSensor  :: HDRSensor
+  , _hdrBloomSettings :: HDRBloomSettings
+  } deriving (Show,Eq,Ord,Data,Typeable,Generic)
+
+data HDRSensor = HDRSensor
+  { _hdrExposure      :: Double
+  , _hdrExposureBias  :: Double
+  , _hdrWhitePoint    :: Double
+  } deriving (Show,Eq,Ord,Data,Typeable,Generic)
 
 data HDRBloomSettings = HDRBloomSettings
-    { _bloomPreDownsampling :: Int
-    , _bloomGaussPasses     :: Int
-    , _bloomFactor          :: Double
-    , _bloomThreshold       :: Double
-    -- ^ ~ 0.5 - 0.8 [Kawase04, Page 29]
-    , _bloomWidth           :: Double
-    } deriving (Show,Eq,Ord,Data,Typeable,Generic)
+  { _bloomPreDownsampling :: Int
+  , _bloomGaussPasses     :: Int
+  , _bloomFactor          :: Double
+  , _bloomThreshold       :: Double
+  -- ^ ~ 0.5 - 0.8 [Kawase04, Page 29]
+  , _bloomWidth           :: Double
+  } deriving (Show,Eq,Ord,Data,Typeable,Generic)
 
 makeClassy ''HDRBloomSettings
-makeClassyFor "HasHDRCamera" "hdrCamera"
-  [ ("_hdrExposure", "exposure")
+
+makeClassyFor "HasHDRSensor" "hdrSensor"
+  [ ("_hdrExposure"    , "exposure")
   , ("_hdrExposureBias", "exposureBias")
-  , ("_hdrWhitePoint", "whitePoint")
+  , ("_hdrWhitePoint"  , "whitePoint")
+  ] ''HDRSensor
+
+makeClassyFor "HasHDRCamera" "hdrCamera"
+  [ ("_hdrCameraSensor" , "cameraSensor")
   , ("_hdrBloomSettings", "bloomSettings")
   ] ''HDRCamera
 
 defaultBloomSettings :: HDRBloomSettings
 defaultBloomSettings = HDRBloomSettings
-    { _bloomPreDownsampling = 2
-    , _bloomGaussPasses     = 7
-    , _bloomFactor          = 0.3
-    , _bloomThreshold       = 0.6
-    , _bloomWidth           = 1.0
-    }
+  { _bloomPreDownsampling = 2
+  , _bloomGaussPasses     = 7
+  , _bloomFactor          = 0.3
+  , _bloomThreshold       = 0.6
+  , _bloomWidth           = 1.0
+  }
 
 defaultHDRCamera :: Camera -> HDRCamera
 defaultHDRCamera cam = HDRCamera
-    { _hdrCameraHandle  = cam
-    , _hdrExposure      = 0.5
+  { _hdrCameraHandle  = cam
+  , _hdrCameraSensor  = HDRSensor
+    { _hdrExposure      = 0.5
     , _hdrExposureBias  = 1.0
     , _hdrWhitePoint    = 0.5
-    , _hdrBloomSettings = defaultBloomSettings
     }
+  , _hdrBloomSettings = defaultBloomSettings
+  }
 
 instance HasCamera HDRCamera where
-    camera = lens _hdrCameraHandle (\s c -> s{_hdrCameraHandle = c})
+  camera = lens _hdrCameraHandle (\s c -> s{_hdrCameraHandle = c})
+
+instance HasHDRSensor HDRCamera where
+  hdrSensor = lens _hdrCameraSensor (\s c -> s{_hdrCameraSensor = c})
 
 instance LinearInterpolatable HDRBloomSettings where
-    lerp alpha u v =
-        u & bloomPreDownsampling .~ v^.bloomPreDownsampling
-          & bloomGaussPasses     .~ v^.bloomGaussPasses
-          & bloomFactor          .~ lerp alpha (u^.bloomFactor) (v^.bloomFactor)
+  lerp alpha u v =
+    u & bloomPreDownsampling .~ v^.bloomPreDownsampling
+      & bloomGaussPasses     .~ v^.bloomGaussPasses
+      & bloomFactor          .~ lerp alpha (u^.bloomFactor) (v^.bloomFactor)
 
 instance LinearInterpolatable HDRCamera where
-    lerp alpha u v =
-        u & camera        .~ lerp alpha (u^.camera) (v^.camera)
-          & exposure      .~ lerp alpha (u^.exposure) (v^.exposure)
-          & exposureBias  .~ lerp alpha (u^.exposureBias) (v^.exposureBias)
-          & whitePoint    .~ lerp alpha (u^.whitePoint) (v^.whitePoint)
-          & bloomSettings .~ lerp alpha (u^.bloomSettings) (v^.bloomSettings)
+  lerp alpha u v = u
+    & camera      .~ lerp alpha (u^.camera) (v^.camera)
+    & hdrSensor     .~ lerp alpha (u^.hdrSensor) (v^.hdrSensor)
+    & bloomSettings .~ lerp alpha (u^.bloomSettings) (v^.bloomSettings)
+
+instance LinearInterpolatable HDRSensor where
+  lerp alpha u v = u
+    & exposure      .~ lerp alpha (u^.exposure) (v^.exposure)
+    & exposureBias  .~ lerp alpha (u^.exposureBias) (v^.exposureBias)
+    & whitePoint    .~ lerp alpha (u^.whitePoint) (v^.whitePoint)
 
 instance Default HDRBloomSettings where
-    def = defaultBloomSettings
+  def = defaultBloomSettings
