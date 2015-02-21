6541fee2bd32b26965099feb24b8a6736cccad86
Jan-Philip Loos <maxdaten@gmail.com>
2015-02-21 09:45:09 +0100

    simple wrapper ctr

--- src/Yage/Uniform/UniformVar.hs
+++ src/Yage/Uniform/UniformVar.hs
@@ -1,5 +1,10 @@
 module Yage.Uniform.UniformVar where
 
+import Yage.Prelude
 import Quine.StateVar
 
 type UniformVar = SettableStateVar
+
+
+mkUniformVar :: (a -> IO ()) -> SettableStateVar a
+mkUniformVar = SettableStateVar
