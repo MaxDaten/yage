aad34215f21ccb2adcf5841a1cbd48b566b36958
Jan-Philip Loos <maxdaten@gmail.com>
2015-03-05 23:33:33 +0100

    indent

--- src/Yage/Rendering/Resources/GL.hs
+++ src/Yage/Rendering/Resources/GL.hs
@@ -7,10 +7,11 @@ module Yage.Rendering.Resources.GL
   , module Texture
   ) where
 
-import           Yage.Rendering.Resources.GL.Base         as Base
-import           Yage.Rendering.Resources.GL.Buffer       as Buffer
-import           Yage.Rendering.Resources.GL.Framebuffer  as Framebuffer
-import           Yage.Rendering.Resources.GL.Renderbuffer as Renderbuffer
-import           Yage.Rendering.Resources.GL.Shader       as Shader
-import           Yage.Rendering.Resources.GL.Texture      as Texture
+import           Yage.Rendering.Resources.GL.Base          as Base
+import           Yage.Rendering.Resources.GL.Buffer        as Buffer
+import           Yage.Rendering.Resources.GL.Framebuffer   as Framebuffer
+import           Yage.Rendering.Resources.GL.Renderbuffer  as Renderbuffer
+import           Yage.Rendering.Resources.GL.Shader        as Shader
+import           Yage.Rendering.Resources.GL.Texture       as Texture
+import           Yage.Rendering.Resources.GL.TextureFormat as Texture
 -- import Yage.Resources.Texture
